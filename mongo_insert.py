d74768f021902009a4ee14502cb648079b96a89b
cl <clearthecity@gmail.com>
2020-03-01 11:55:05 -0800

    delete working file

--- mongo_insert.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-from decouple import config
-
-connection_string = config('MONGO_CONNECTION_STRING')
-client = pymongo.MongoClient(connection_string)
-db = client.db
-coll = db["twitter"]
-
-coll.insert_many()
